e4dcf0df030e570b602fe8b046bbf6201a97663a
ikarkam76 <ikarkam76@gmail.com>
2023-01-24 00:23:32 +0200

    add path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
