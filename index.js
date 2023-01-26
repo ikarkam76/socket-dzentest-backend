d2253ce9eb360faccf5cc34c1ff7935a88527cef
ikarkam76 <ikarkam76@gmail.com>
2023-01-26 09:39:00 +0200

    change app use static

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: "*"
   }));
-  app.use(express.static(path.join(__dirname, "server")));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
